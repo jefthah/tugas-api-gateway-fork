2f65baea59607abf4edf5753b5418ce3783898cf
daffaYuharshu <daffayuharshu@gmail.com>
2024-05-23 23:30:17 +0700

    add navigate variable

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Student from './components/Student';
 import Navbar from './components/Navbar';
 import Subject from './components/Subject';
